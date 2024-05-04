63ba93d7e3d1619d8a4b65fcf41c6e4a027d54e9
Jacob <jacob.moujalli@hotmail.com>
2024-05-04 21:36:53 +1000

    * emacs/.emacs.d/early-init.el: Fixed background colour.

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,11 +1,10 @@
 ;; Set colours early
-(setq initial-frame-alist '((background-color . "black")))
+(setq initial-frame-alist '((background-color . "#181818")))
 
 ;; Standard config
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
-(column-number-mode 1)
 (show-paren-mode 1)
 
 ;; Start maximized
