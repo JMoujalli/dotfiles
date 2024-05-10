5ae785ff34a719c9e2b81725be01bc446e07c378
Jacob Moujalli <Jacob.Moujalli@hotmail.com>
2024-05-10 19:02:44 +1000

    Fixed empty string case.
    * i3/.config/i3/screenselect.sh:

--- i3/.config/i3/screenselect.sh
+++ i3/.config/i3/screenselect.sh
@@ -4,6 +4,7 @@ choices="$(ls ~/.screenlayout/)\nDPI\nManual"
 chosen=$(echo -e "$choices" | dmenu -i)
 
 case "$chosen" in
+    "") ~/.config/i3/screenlayout.sh ;;
     Manual) arandr ;;
     DPI) ~/.config/i3/dpi.sh ;;
     *) cat ~/.screenlayout/$chosen > ~/.config/i3/screenlayout.sh
