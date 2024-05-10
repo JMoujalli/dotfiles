a2475b4720e62824d166faa96e7a4615b32d139b
Jacob Moujalli <Jacob.Moujalli@hotmail.com>
2024-05-10 18:54:05 +1000

    Typo in i3 config.
    * README.org:

--- i3/.config/i3/screenlayout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate right --output DP-2 --primary --mode 1920x1080 --pos 1080x420 --rotate normal --output DP-3 --off --output HDMI-1 --off --output DP-4 --off --output DP-5 --off
