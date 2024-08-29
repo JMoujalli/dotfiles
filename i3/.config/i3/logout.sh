0e9e9a3ce5fedbe98a2deace691ea77bc51ba5d5
Jacob Moujalli <Jacob.Moujalli@hotmail.com>
2024-08-29 21:06:04 +1000

    Applets, Suspend, Random.
    * emacs/.emacs.d/init.el:

--- i3/.config/i3/logout.sh
+++ i3/.config/i3/logout.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-choices="Shutdown\nReboot"
+choices="Suspend\nShutdown\nReboot"
 
 chosen=$(echo -e "$choices" | dmenu -i)
 
 case "$chosen" in
+    Suspend) systemctl suspend ;;
     Shutdown) shutdown now ;;
     Reboot) reboot ;;
 esac
