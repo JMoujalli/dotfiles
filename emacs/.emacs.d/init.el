57016c393128599ee729270ffc498467cf5ee7cc
Jacob Moujalli <Jacob.Moujalli@hotmail.com>
2026-05-19 02:20:10 +1000

    Emacs ef-themes
    * emacs/.emacs.d/init.el:

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -373,7 +373,30 @@ skip exactly those headlines that do not match."
 (setq custom-file (make-temp-file "emacs-custom-"))
 
 (setq custom-safe-themes t)
-;; (use-package gruber-darker-theme :ensure t)
-;; (load-theme 'gruber-darker t)
-(global-set-key (kbd "C-x t") 'modus-themes-toggle)
-(load-theme 'modus-vivendi t)
+
+(use-package ef-themes
+  :ensure t
+  :init
+  ;; This makes the Modus commands listed below consider only the Ef
+  ;; themes.  For an alternative that includes Modus and all
+  ;; derivative themes (like Ef), enable the
+  ;; `modus-themes-include-derivatives-mode' instead.  The manual of
+  ;; the Ef themes has a section that explains all the possibilities:
+  ;;
+  ;; - Evaluate `(info "(ef-themes) Working with other Modus themes or taking over Modus")'
+  ;; - Visit <https://protesilaos.com/emacs/ef-themes#h:6585235a-5219-4f78-9dd5-6a64d87d1b6e>
+  (ef-themes-take-over-modus-themes-mode 1)
+  :bind
+  (("C-x t s" . modus-themes-select))
+  :config
+  ;; All customisations here.
+  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-italic-constructs t)
+
+  ;; Finally, load your theme of choice (or a random one with
+  ;; `modus-themes-load-random', `modus-themes-load-random-dark',
+  ;; `modus-themes-load-random-light').
+  (modus-themes-load-theme 'ef-bio))
+
+;; NOTE: May want to set a dark theme and a light theme default. Have "C-x t t" toggle the theme between light and dark.
+;; May want to add parity between emacs themes and the i3/GTK/Qt themes at a later point. In the mean time probably just have the default themes match i3.
